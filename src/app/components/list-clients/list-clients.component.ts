9fe0791b6ce7aa0d7c56b334f2c024c12088b48c
DanielSales <daniel-silvasales@hotmail.com>
2024-09-12 00:32:56 -0300

    '12-09-20240032'

--- src/app/components/list-clients/list-clients.component.ts
+++ src/app/components/list-clients/list-clients.component.ts
@@ -1,36 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router'
 import { CorbanService } from '../../services/corban.service';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule }from '@angular/material/sidenav';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { clienteLiteInterface } from '../../interfaces/clienteLite';
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-list-clients',
@@ -44,6 +23,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatIconModule,
     MatFormFieldModule,
     MatTableModule,
+    MatInputModule,
     MatToolbarModule
   ],
   templateUrl: './list-clients.component.html',
@@ -52,8 +32,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class ListClientsComponent implements OnInit {
 
-  
-  
+  showFiller = true;
+
   constructor(private _corbanService : CorbanService, private _toastr:ToastrService){
   }
 
