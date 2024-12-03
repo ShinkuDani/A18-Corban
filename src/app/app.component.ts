e39b0fd15c2e7ec9c62287a048db5302c85e62df
Daniel Silva Sales <daniel.ssales@itavema.int>
2024-12-03 16:26:26 -0300

    '03-12-20241626'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CorbanService } from './services/corban.service';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +20,9 @@ import { MatMenuModule } from '@angular/material/menu';
     MatFormFieldModule,
     MatButtonModule,
     MatMenuModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
 
    ],
   templateUrl: './app.component.html',
@@ -24,4 +30,35 @@ import { MatMenuModule } from '@angular/material/menu';
 })
 export class AppComponent {
   title = 'app';
+
+  
+constructor(private _corbanService:CorbanService, private _router: Router){}
+
+continuar:boolean = false;
+usuarioNome = new FormControl('daniel-silvasales@hotmail.com');
+usuarioSenha = new FormControl('DanielTesteCorban360');
+
+gettingToken(){
+  this._corbanService.getToken2(this.usuarioNome.value ?? '', this.usuarioSenha.value ?? '').subscribe(
+    data => {
+      if(data){
+        localStorage.setItem('token', data.token);
+        console.log('Login Realizado com Sucesso');
+        this.continuar = true;
+        this._router.navigate(['/listaClientes']);
+      }else{
+        window.alert('Não foi possivel Fazer Login')
+        alert('Não foi possivel Fazer Login')
+        console.log('Não foi possivel fazer o Login')
+        this.usuarioSenha.reset()
+      }
+    }
+  )
+}
+
+logOff(){
+  this.continuar = false;
+  this._router.navigate(['']);
+}
+
 }
