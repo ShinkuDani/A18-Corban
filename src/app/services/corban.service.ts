50cf0f210ad176f8ac2986d258a02c36a849d0bf
Daniel Silva Sales <72050361+ShinkuDani@users.noreply.github.com>
2025-11-10 12:58:36 -0300

    Update authorization header to use token variable

--- src/app/services/corban.service.ts
+++ src/app/services/corban.service.ts
@@ -9,9 +9,10 @@ import { LoginUser } from '../../loginUser';
 export class CorbanService {
 
   loginUser = new LoginUser()
+  token = ''
 
    headers = new HttpHeaders()
-  .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiN2QxMzA2ZTAtZTUxZS00ODY2LWEzZmQtZjM4MjE3ZGQ1MmIyIiwic2Vzc2lvbl9pZCI6ImNmMTEzZDQ2LWE3ZTQtNDRiMS04ZTg2LThlZmJjNmNiNjFiMiIsInNpZ25hbF9pZCI6ImVlY2Y0NDZmLWM1ZGEtNDAwMS1hYjE4LTIyMWZlODYzYjhmZiIsImhvc3QiOiJodHRwczovL2FwaTQuYWppbi5pby9pbmRleC5odG1sIiwiaXAiOiIxOTEuMTgzLjM4LjQyIiwibmJmIjoxNzIxODY2MDkxLCJleHAiOjE3MjE5NTI0OTEsImlhdCI6MTcyMTg2NjA5MX0.98mibH3gHtNqltS1IP0qrMYeh9NMK5ZEypPdCa472Ko');
+  .set('Authorization', ´Bearer ${token}´ );
 
 
   constructor(private http: HttpClient) { }
